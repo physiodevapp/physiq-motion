1f0104b386c2db4a4a348d11c923bb1ed08ea745
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2026-05-22 20:50:44 +0200

    reposition movement badge to top-right aligned with movement label

--- app.js
+++ app.js
@@ -259,12 +259,14 @@ function buildCard(id, def, val, i) {
   const btnLabel = val !== null ? 'Repetir' : 'Medir';
 
   card.innerHTML = `
-    <div>
-      <div class="mov-label">${def.label}</div>
-      <div class="mov-ref">Ref: ${def.ref}°</div>
+    <div class="mov-top">
+      <div>
+        <div class="mov-label">${def.label}</div>
+        <div class="mov-ref">Ref: ${def.ref}°</div>
+      </div>
+      ${badgeHtml}
     </div>
     ${valueHtml}
-    <div class="mov-badge">${badgeHtml}</div>
     <button class="${btnCls}" onclick="openMeasurement('${id}')">${btnLabel}</button>`;
   return card;
 }
